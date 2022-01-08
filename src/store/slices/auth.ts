ce8f280798757430384e0a0d78b6e2c3dee4618a
myk4040okothogodo <mikeogodo5@gmail.com>
2022-01-08 23:26:21 +0300

    inception

---
+++ src/store/slices/auth.ts
@@ -0,0 +1,37 @@
+/* With Redux, a slice is a collection of reducer logic and actions for a single for a single feature of our
+ * app*/
+
+import {  createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { AccountResponse } from "../../types";
+
+type State = {
+  token: string | null;
+  refreshToken: string | null;
+  account: AccountResponse | null;
+
+};
+
+const initialState: State = {token: null, refreshToken: null, account:null};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuthTokens(
+      state: State,
+      action: PayloadAction<{ token: string; refreshToken: string }>
+    ) {
+      state.refreshToken = action.payload.refreshToken;
+      state.token = action.payload.token;
+    },
+    setAccount(state: State, action: PayloadAction<AccountResponse>){
+      state.account = action.payload;
+    },
+    logout(state:State){
+       state.account = null;
+       state.refreshToken = null;
+       state.token = null;
+    },
+  },
+});
+export default authSlice;
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface AccountResponse {
+  user: {
+    id: string;
+    email: string;
+    username: string;
+    is_active: boolean;
+    created: Date;
+    updated: Date;
+  };
+  access: string;
+  refresh: string;
+
+}
